bffa9efd9293d44c88645fb3164b3a5fc815e40a
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2023-01-28 14:29:39 -0300

    Final commit project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\ReasonContact;
 
 class HomeController extends Controller
 {
     public function homeIndex(){
-        return view('app.home');
+        $reason_contact = ReasonContact::all();
+        return view('app.home', ['reason_contact' => $reason_contact]);
     }
 }
--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -1,8 +1,44 @@
 
 @extends('layout.basicApp')
 
-@section('title', 'Home')
-    
 @section('content')
-    <br> <br> HOME
+
+
+{{-- Call of partials/top.blade.php: --}}
+{{-- @include('partials.top') --}}
+
+<div class="conteudo-destaque">
+
+    <div class="esquerda">
+        <div class="informacoes">
+            <h1>Sistema Gestão Fácil</h1>
+            <p>Software para gestão empresarial ideal para sua empresa.<p>
+            <p>Facilite seus processos!<p>
+            <div class="chamada">
+                <img src="{{asset('img/check.png')}}">
+                <span class="texto-branco">Gestão completa e descomplicada</span>
+            </div>
+            <div class="chamada">
+                <img src="{{asset('img/check.png')}}">
+                <span class="texto-branco">Sua empresa na nuvem</span>
+            </div>
+        </div>
+
+        <div class="video">
+            <img src="{{asset('img/player_video.jpg')}}">
+        </div>
+    </div>
+
+    <div class="direita">
+        <div class="contato">
+            <h1>Contato</h1>
+            <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
+            
+                @component('components.form', ['reason_contact' => $reason_contact])
+                @endcomponent
+
+        </div>
+    </div>
+</div>
+
 @endsection
